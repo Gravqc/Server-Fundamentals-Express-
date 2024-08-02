f75c9ccb66b0a7e04e83ab836c79a1dd3d34629d
Gravqc <gauravgowda452@gmail.com>
2024-08-02 16:06:34 +0530

    trying out get methods

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// these are all routes that the user will query the server to do something
+// "/"=> base url so when the client accesses the base url then the server will be sending hello world
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "Hellow" });
+});
+
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, userName: "Gaurav", email: "email.com" },
+    { id: 1, userName: "Gaurav", email: "email.com" },
+    { id: 1, userName: "Gaurav", email: "email.com" },
+  ]);
+});
 
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
